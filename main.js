0c7b9354abe281a29540449d95511f0867ef743c
Juan Ramirez <rami8224.2007@gmail.com>
2022-04-06 11:21:04 -0400

    completed part2

--- main.js
+++ main.js
@@ -2,21 +2,40 @@
 //get random image
 let img = document.querySelector('.card-img-top')
 let button = document.querySelector('.btn')
-// console.log(img)
-//return http response
+//add button listener
 button.addEventListener('click',()=> {
     fetch("https://dog.ceo/api/breeds/image/random")
-    .then(function(response){
-        // console.log(response)
-        return response.json()
-    })
-    .then(function(data){
+    .then((data)=>data.json())
+    .then((res)=> {
         //set the image with the data MESSAGE
-        img.src = data.message
-    }) 
+        console.log(res)
+        img.src = res.message
+    })
+    //logs exceptions if 
+    .catch((error)=> console.log(error)) 
 })
 
-
-//button changes pictures of dogs
-
-// console.log(button)
+//weather
+let getWeather = document.querySelector("#getWeather")
+let cityText = document.querySelector('#cityName')
+// console.log(getWeather)
+let temp = document.querySelector('#temperature')
+let wind = document.querySelector('#wind')
+let description = document.querySelector('#description')
+console.log(temp)
+getWeather.addEventListener('click',()=>{
+    
+    let nameVal = `https://goweather.herokuapp.com/weather/${cityText.value}`
+    // console.log(cityText.value)
+    fetch(`${nameVal}`)
+        .then((data) => {
+            // console.log(data)
+            return data.json()
+        })
+        .then((res)=>{
+            console.log(res)
+            temp.textContent= res.temperature
+            wind.textContent= res.wind
+            description.textContent= res.description
+        })
+})
